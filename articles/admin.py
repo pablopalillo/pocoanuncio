549e775ece96e046915ad675c6fa33727af7b72e
pablopalillo <pablo.576@hotmail.com>
2021-10-15 19:35:24 -0500

    App Articles changes:     - Model Category     - Model Articles     - Admin Cofiguration

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Article
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('id')
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    readonly_fields = ('id')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,48 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class StatusChoices(models.TextChoices):
+        """
+            Estructure options for Subcategories status
+        """
+
+        ACTIVE = '1', _('Active')
+        INNACTIVE = '2', _('Innactive')
+
+    id = models.AutoField(primary_key=True)
+    subcategory = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        related_name='subcategories',
+        null=True
+    )
+    name = models.CharField(max_length=15)
+    status = models.CharField(
+        max_length=1,
+        choices=StatusChoices.choices,
+        default=StatusChoices.ACTIVE,
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Article(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='article'
+    )
+    title = models.CharField(max_length=35)
+    description = models.TextField(null=True)
+    img_url = models.CharField(max_length=200, null=True)
+
+    def __str__(self) -> str:
+        return self.title
