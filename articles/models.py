d0ae78769651ffc5647c6da378095c21b516e272
pablopalillo <pablo.576@hotmail.com>
2021-10-16 16:34:48 -0500

    Fix max length for title of categories

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,44 @@
 from django.utils.translation import gettext_lazy as _
+from helpers.scraper import Scraper
 from django.db import models
+
+
+class CategoryManagerFillData(models.Manager):
+    """
+    Manager for Category Model this fill data for Categories,
+    using the srapping method.
+
+    Params:
+    web_url : str origin valid url
+    """
+
+    web_url = "https://www.milanuncios.com/"
+
+    def save_scrap_data(self) -> None:
+        web_dom = Scraper(self.web_url)
+        element_class = "ma-MainCategory-mainCategoryNameLink"
+        titles_cat_dom = web_dom.get_list_elements_by_class(element_class)
+        url_cat_dom = web_dom.get_list_elements_by_attr(element_class, "href")
+        categoy_objects = self._fill_category_data(titles_cat_dom, url_cat_dom)
+
+        if categoy_objects:
+            self.save_categories(categoy_objects)
+
+    def _fill_category_data(self, titles: list, urls: list) -> list:
+        """ Fill list of Categories Model objects"""
+        category_objects = []  # list of objects
+
+        if titles and urls:
+            for index, title in enumerate(titles):
+                category_objects.append(Category(name=title, slug=urls[index]))
+        return category_objects
+
+    def save_categories(self, categories: list) -> None:
+        """ Massive insert of categories"""
+
+        if categories:
+            size_list = len(categories)
+            Category.objects.bulk_create(categories, size_list)
 
 
 class Category(models.Model):
@@ -19,13 +58,14 @@ class Category(models.Model):
         related_name='subcategories',
         null=True
     )
-    name = models.CharField(max_length=15)
+    name = models.CharField(max_length=30)
     slug = models.SlugField(max_length=400)
     status = models.CharField(
         max_length=1,
         choices=StatusChoices.choices,
         default=StatusChoices.ACTIVE,
     )
+    objects = CategoryManagerFillData()
 
     def __str__(self) -> str:
         return self.name
