80bcbe262da6940f261148fcd96e66303efbeea1
pablopalillo <pablo.576@hotmail.com>
2021-10-16 00:31:55 -0500

    Scraper helper class    - Use the library BeautifulSoup for srap pages    - validate if the url is well formed    - function dom elements by class for get especific element

---
+++ helpers/Scraper.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
+
+class Scraper:
+    """
+    Scraper Tool Class for obtain doom HTML information for available
+    web page.
+
+    Attributes
+    ----------
+    url : str
+        Valid URL format of active web page
+    dom_parser : str
+        Content DOM of web page set in the URL
+    """
+
+    ulr = None
+    dom_parser = None
+
+    def __init__(self, url: str) -> None:
+        self.url = url
+        self.get_dom_web_page()
+
+    def get_dom_web_page(self) -> None:
+        """Parser DOM information and set in dom_parser attribute"""
+        if(self._validate_url()):
+            page_request = requests.get(self.url)
+            self.dom_parser = BeautifulSoup(
+                page_request.content,
+                'html.parser'
+            )
+
+    def get_dom_elements_by_class(self, class_element: str) -> list:
+        """
+        Get DOM especific repetitive elements and return a list
+
+        Params:
+        class_element : str Name of class o DOM element
+
+        Rerturn:
+        list of repetitive elements with the same class
+        """
+        items_founds_dom = None
+        if class_element is not None:
+            items_founds_dom = self.dom_parser.find_all(
+                "a",
+                class_=class_element
+            )
+
+        return items_founds_dom
+
+    def _validate_url(self) -> bool:
+        """ Validate if the url is well formed """
+        validator = URLValidator()
+        try:
+            validator(self.url)
+        except ValidationError as e:
+            print(e)
+            return False
+
+        return True
