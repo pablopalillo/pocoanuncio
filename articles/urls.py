3d5f4ecc71a1eb4818da9bbcaf81ad232ebfdaaf
pablopalillo <pablo.576@hotmail.com>
2021-10-16 23:35:09 -0500

    View articles and index

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
+   path('', views.IndexView.as_view()),
    path('import', views.ImportScrapDataView.as_view()),
 ]
--- articles/views.py
+++ articles/views.py
@@ -3,8 +3,14 @@ from django.views import View
 from .models import Category, Article
 
 
+class IndexView(View):
+
+    def get(self, request):
+        return render(request, "page-index.html")
+
+
 class ImportScrapDataView(View):
 
     def get(self, request):
-        Article.objects.save_scrap_data()
+        Category.objects.save_scrap_data()
         return "done"
